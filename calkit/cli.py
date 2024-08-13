9f07a123e8196ef5640e2fbf20fcf06702f86f34
Pete Bachant <petebachant@gmail.com>
2024-08-13 08:25:22 -0700

    Add basic status command to CLI

--- calkit/cli.py
+++ calkit/cli.py
@@ -1,5 +1,9 @@
 """The command line interface."""
 
+import os
+import pty
+import subprocess
+
 import typer
 
 from . import config
@@ -29,6 +33,26 @@ def set_config_value(key: str, value: str):
 def get_config_value(key: str) -> None:
     cfg = config.read()
     print(getattr(cfg, key))
+
+
+@app.command(name="status")
+def get_status():
+    """Get a unified Git and DVC status."""
+
+    def print_sep(name: str):
+        print(f"------------ {name} ------------")
+
+    print_sep("Code")
+    if os.name == "nt":
+        subprocess.call(["git", "status"])
+        print()
+        print_sep("data")
+        subprocess.call(["dvc", "status"])
+    else:
+        pty.spawn(["git", "status"], lambda fd: os.read(fd, 1024))
+        print()
+        print_sep("Data")
+        pty.spawn(["dvc", "status"], lambda fd: os.read(fd, 1024))
 
 
 def run() -> None:

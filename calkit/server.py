c18ec7513c73f824ec01d8edf198af873188fa72
Pete Bachant <petebachant@gmail.com>
2024-08-18 18:00:50 -0700

    Add server endpoint for running git commands

--- calkit/server.py
+++ calkit/server.py
@@ -2,6 +2,7 @@
 
 import os
 
+import git
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -24,3 +25,9 @@ def get_health() -> str:
 @app.get("/cwd")
 def get_cwd() -> str:
     return os.getcwd()
+
+
+@app.post("/git/{command}")
+def run_git_command(command: str, params: dict = {}):
+    func = getattr(git.Repo().git, command)
+    return func(**params)

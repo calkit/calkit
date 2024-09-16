b6bf37c7655a9c8639c390b3f758cb3bc1a22ecd
Pete Bachant <petebachant@gmail.com>
2024-09-16 06:33:39 -0700

    Use app name for DVC remote

--- calkit/dvc.py
+++ calkit/dvc.py
@@ -3,8 +3,7 @@
 import subprocess
 
 import calkit
-
-REMOTE_NAME = "local"  # TODO: Use package name
+from calkit.config import get_app_name
 
 
 def configure_remote():
@@ -12,14 +11,19 @@ def configure_remote():
     base_url = calkit.cloud.get_base_url()
     remote_url = f"{base_url}/projects/{project_name}/dvc"
     subprocess.call(
-        ["dvc", "remote", "add", "-d", "-f", REMOTE_NAME, remote_url]
+        ["dvc", "remote", "add", "-d", "-f", get_app_name(), remote_url]
     )
-    subprocess.call(["dvc", "remote", "modify", REMOTE_NAME, "auth", "custom"])
+    subprocess.call(
+        ["dvc", "remote", "modify", get_app_name(), "auth", "custom"]
+    )
 
 
 def set_remote_auth():
     """Get a token and set it in the local config so we can interact with
     the API.
+
+    # TODO: This should probably create a longer-lived token and perhaps not
+    # use a stored password, despite it being stored in the system keyring.
     """
     token = calkit.cloud.auth()
     subprocess.call(
@@ -28,7 +32,7 @@ def set_remote_auth():
             "remote",
             "modify",
             "--local",
-            REMOTE_NAME,
+            get_app_name(),
             "custom_auth_header",
             "Authorization",
         ]
@@ -39,7 +43,7 @@ def set_remote_auth():
             "remote",
             "modify",
             "--local",
-            REMOTE_NAME,
+            get_app_name(),
             "password",
             f"Bearer {token}",
         ]

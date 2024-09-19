176c1569cc1a18b6923990ac0d0ba404a19ce57c
Pete Bachant <petebachant@gmail.com>
2024-09-19 10:16:01 -0700

    Update docstring

--- calkit/core.py
+++ calkit/core.py
@@ -5,7 +5,7 @@ import os
 
 
 def find_project_dirs(relative=False, max_depth=3) -> list[str]:
-    """Find all Calkit projects under the current user directory."""
+    """Find all Calkit project directories."""
     if relative:
         start = ""
     else:

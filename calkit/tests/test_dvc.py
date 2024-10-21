e59b37cc469ba43a69e3c6fe5ef100ce0ce74f7e
Pete Bachant <petebachant@gmail.com>
2024-10-21 07:43:53 -0700

    Add function to get DVC remotes

---
+++ calkit/tests/test_dvc.py
@@ -0,0 +1,15 @@
+"""Tests for the ``dvc`` module."""
+
+import subprocess
+
+import calkit
+
+
+def test_get_remotes(tmp_dir):
+    subprocess.call(["git", "init"])
+    assert not calkit.dvc.get_remotes()
+    subprocess.call(["dvc", "init"])
+    assert not calkit.dvc.get_remotes()
+    subprocess.call(["dvc", "remote", "add", "something", "https://sup.com"])
+    resp = calkit.dvc.get_remotes()
+    assert resp == {"something": "https://sup.com"}

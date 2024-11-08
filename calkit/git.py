6b67524b783f966285fdd0ef30b355860f1d3a92
Pete Bachant <petebachant@gmail.com>
2024-11-08 13:27:21 -0800

    Add TODO note about project names that don't match their GitHub repo names

--- calkit/git.py
+++ calkit/git.py
@@ -8,7 +8,10 @@ import git
 def detect_project_name(path=None) -> str:
     """Read the project owner and name from the remote.
 
-    TODO: Currently only works with GitHub remotes.
+    TODO: Currently only works with GitHub remotes where the GitHub repo
+    name is identical to the Calkit project name, which is not guaranteed.
+    We should probably look inside ``calkit.yaml`` in ``project.name``
+    first, and fallback to the GitHub remote URL if we can't find that.
     """
     url = git.Repo(path=path).remote().url
     return url.split("github.com")[-1][1:].removesuffix(".git")

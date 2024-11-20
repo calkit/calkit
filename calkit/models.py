75838157a6c2996981cc52c9eb88e106e586dc72
Pete Bachant <petebachant@gmail.com>
2024-11-20 04:26:17 -0800

    Add the ability to define a repeated procedure step

--- calkit/models.py
+++ calkit/models.py
@@ -118,6 +118,7 @@ class ProcedureStep(BaseModel):
     wait_before_s: float | None = None
     wait_after_s: float | None = None
     inputs: dict[str, ProcedureInput] | None = None
+    repeat: int | None = None
 
 
 class Procedure(BaseModel):

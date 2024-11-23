381209903d55a23af20583d35159ab9177b37647
Pete Bachant <petebachant@gmail.com>
2024-11-23 05:52:19 -0800

    Add to test

--- calkit/tests/test_magics.py
+++ calkit/tests/test_magics.py
@@ -1,11 +1,22 @@
 """Tests for ``calkit.magics``."""
 
+import os
+import shutil
 import subprocess
+
 
 def test_stage(tmp_dir):
     # Test the stage magic
     # Run git and dvc init in the temp dir
     subprocess.check_call(["git", "init"])
     subprocess.check_call(["dvc", "init"])
-    # TODO: Copy in a test notebook and run it
-    pass
+    # Copy in a test notebook and run it
+    nb_fpath = os.path.join(
+        os.path.dirname(__file__), "..", "..", "test", "pipeline.ipynb"
+    )
+    shutil.copy(nb_fpath, "notebook.ipynb")
+    subprocess.check_call(
+        ["jupyter", "nbconvert", "--execute", "notebook.ipynb", "--to", "html"]
+    )
+    # TODO: Check DVC stages make sense
+    # TODO: Check Calkit metadata makes sense

8b5427da03d1a1f25787c9d454035d4ef5b5a6a8
Pete Bachant <petebachant@gmail.com>
2024-12-08 09:20:04 -0800

    Run conda commands in a shell

--- calkit/cli/main.py
+++ calkit/cli/main.py
@@ -584,11 +584,20 @@ def run_in_env(
         except subprocess.CalledProcessError:
             raise_error("Failed to run in Docker environment")
     elif env["kind"] == "conda":
+        shell_cmd = " ".join(cmd)
         with open(env["path"]) as f:
             conda_env = calkit.ryaml.load(f)
         if not no_check:
             check_conda_env(env_fpath=env["path"], relaxed=relaxed_check)
-        cmd = ["conda", "run", "-n", conda_env["name"]] + cmd
+        cmd = [
+            "conda",
+            "run",
+            "-n",
+            conda_env["name"],
+            "/bin/sh",
+            "-c",
+            f"{shell_cmd}",
+        ]
         if verbose:
             typer.echo(f"Running command: {cmd}")
         try:

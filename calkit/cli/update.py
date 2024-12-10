3bc9e37cbf656ea790512b7b74c43938d80ffb88
Pete Bachant <petebachant@gmail.com>
2024-12-10 06:04:44 -0800

    Annotate wdir option in update devcontainer

--- calkit/cli/update.py
+++ calkit/cli/update.py
@@ -6,12 +6,24 @@ import os
 
 import requests
 import typer
+from typing_extensions import Annotated
 
 update_app = typer.Typer(no_args_is_help=True)
 
 
 @update_app.command(name="devcontainer")
-def update_devcontainer(wdir: str | None = None):
+def update_devcontainer(
+    wdir: Annotated[
+        str,
+        typer.Option(
+            "--wdir",
+            help=(
+                "Working directory. "
+                "By default will run current working directory."
+            ),
+        ),
+    ] = None,
+):
     """Update a project's devcontainer to match the latest Calkit spec."""
     url = (
         "https://raw.githubusercontent.com/calkit/devcontainer/"

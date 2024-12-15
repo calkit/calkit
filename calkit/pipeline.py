95d421b42e0389e9c949993b8b4740ae8a03d2e3
Pete Bachant <petebachant@gmail.com>
2024-12-15 05:46:12 -0800

    Create dummy stage decorator method

--- calkit/pipeline.py
+++ calkit/pipeline.py
@@ -2,6 +2,8 @@
 
 from pydantic import BaseModel
 
+from typing import Callable
+
 
 class Dependency(BaseModel):
     path: str
@@ -24,9 +26,26 @@ class Dataset(Output):
 class Pipeline(BaseModel):
     deps: list[Dependency] | None = None
 
-    def stage(self, func):
+    def stage(self, *args, other_arg=None):
         """A decorator that creates a pipeline stage."""
-        print("Stage being created from", func)
+
+        def decorator(func):
+
+            print("Stage being created from", func)
+            print("Other arg:", other_arg)
+
+            def wrapper(*args, **kwargs):
+                print(f"Calling {func.__name__} with instance {self}...")
+                result = func(*args, **kwargs)
+                print(f"{func.__name__} finished. Result: {result}")
+                return result
+
+            return wrapper
+
+        if len(args) == 1 and callable(args[0]):
+            return decorator(args[0])
+
+        return decorator
 
     def run(self):
         """Run the pipeline."""

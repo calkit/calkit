0a8267841a744e1b66a32bb9edde8868a06e90fb
Pete Bachant <petebachant@gmail.com>
2025-01-25 09:48:18 -0800

    Implement disconnect for SSH jobs

--- calkit/cli/main.py
+++ calkit/cli/main.py
@@ -860,36 +860,76 @@ def run_in_env(
         if key is not None:
             key = os.path.expanduser(os.path.expandvars(key))
         remote_shell_cmd = _to_shell_cmd(cmd)
-        remote_cmd = f"cd '{remote_wdir}' && {remote_shell_cmd}"
+        # Run with nohup so we can disconnect
+        # TODO: Should we collect output instead of send to /dev/null?
+        remote_cmd = (
+            f"cd '{remote_wdir}' ; nohup {remote_shell_cmd} "
+            "> /dev/null 2>&1 & echo $! "
+        )
         key_cmd = ["-i", key] if key is not None else []
-        # First make sure the remote working dir exists
-        typer.echo("Ensuring remote working directory exists")
-        subprocess.check_call(
-            ["ssh"] + key_cmd + [f"{user}@{host}", f"mkdir -p {remote_wdir}"]
-        )
-        # Now send any necessary files
-        if send_paths:
-            typer.echo("Sending to remote directory")
-            # Accept glob patterns
-            paths = []
-            for p in send_paths:
-                paths += glob.glob(p)
-            scp_cmd = (
-                ["scp", "-r"]
+        # Check to see if we've already submitted a job with this command
+        jobs_fpath = ".calkit/jobs.yaml"
+        job_key = f"{env_name}::{remote_shell_cmd}"
+        remote_pid = None
+        if os.path.isfile(jobs_fpath):
+            with open(jobs_fpath) as f:
+                jobs = calkit.ryaml.load(f)
+            if jobs is None:
+                jobs = {}
+        else:
+            jobs = {}
+        job = jobs.get(job_key, {})
+        remote_pid = job.get("remote_pid")
+        if remote_pid is None:
+            # First make sure the remote working dir exists
+            typer.echo("Ensuring remote working directory exists")
+            subprocess.check_call(
+                ["ssh"]
                 + key_cmd
-                + paths
-                + [f"{user}@{host}:{remote_wdir}/"]
+                + [f"{user}@{host}", f"mkdir -p {remote_wdir}"]
             )
+            # Now send any necessary files
+            if send_paths:
+                typer.echo("Sending to remote directory")
+                # Accept glob patterns
+                paths = []
+                for p in send_paths:
+                    paths += glob.glob(p)
+                scp_cmd = (
+                    ["scp", "-r"]
+                    + key_cmd
+                    + paths
+                    + [f"{user}@{host}:{remote_wdir}/"]
+                )
+                if verbose:
+                    typer.echo(f"scp cmd: {scp_cmd}")
+                subprocess.check_call(scp_cmd)
+            # Now run the command
+            # TODO: Use some sort of non-blocking job so we can disconnect and
+            # resume
+            typer.echo(f"Running remote command: {remote_shell_cmd}")
             if verbose:
-                typer.echo(f"scp cmd: {scp_cmd}")
-            subprocess.check_call(scp_cmd)
-        # Now run the command
-        # TODO: Use some sort of non-blocking job so we can disconnect and
-        # resume
-        typer.echo(f"Running remote command: {remote_cmd}")
-        subprocess.check_call(
-            ["ssh"] + key_cmd + [f"{user}@{host}", remote_cmd]
-        )
+                typer.echo(f"Full command: {remote_cmd}")
+            remote_pid = (
+                subprocess.check_output(
+                    ["ssh"] + key_cmd + [f"{user}@{host}", remote_cmd]
+                )
+                .decode()
+                .strip()
+            )
+            typer.echo(f"Running with remote PID: {remote_pid}")
+        # Now wait for the job to complete
+        typer.echo(f"Waiting for remote PID {remote_pid} to finish")
+        ps_cmd = ["ssh"] + key_cmd + [f"{user}@{host}", "ps", "-p", remote_pid]
+        finished = False
+        while not finished:
+            try:
+                subprocess.check_output(ps_cmd)
+                finished = False
+                time.sleep(2)
+            except subprocess.CalledProcessError:
+                finished = True
+                typer.echo("Remote process finished")
         # Now sync the files back
         # TODO: Figure out how to do this in one command
         # Getting the syntax right is troublesome since it appears to work
@@ -901,6 +941,14 @@ def run_in_env(
                 src = f"{user}@{host}:{src_path}"
                 scp_cmd = ["scp", "-r"] + key_cmd + [src, "."]
                 subprocess.check_call(scp_cmd)
+        # Now delete the remote PID from the jobs file
+        typer.echo("Updating jobs database")
+        os.makedirs(".calkit", exist_ok=True)
+        job["remote_pid"] = None
+        job["last_finished"] = time.time()
+        jobs[job_key] = job
+        with open(jobs_fpath, "w") as f:
+            calkit.ryaml.dump(jobs, f)
     else:
         raise_error("Environment kind not supported")
 

05fa564ef431882169f551a297e6613e77219d86
Pete Bachant <petebachant@gmail.com>
2025-02-18 05:20:30 -0800

    Add test for detect_project_name

--- calkit/tests/test_core.py
+++ calkit/tests/test_core.py
@@ -1,5 +1,9 @@
 """Tests for the ``core`` module."""
 
+import subprocess
+
+import git
+
 import calkit
 
 
@@ -11,3 +15,13 @@ def test_find_project_dirs():
 def test_to_kebab_case():
     assert calkit.to_kebab_case("THIS IS") == "this-is"
     assert calkit.to_kebab_case("this_is_my-Project") == "this-is-my-project"
+
+
+def test_detect_project_name(tmp_dir):
+    subprocess.check_output(["calkit", "init"])
+    repo = git.Repo()
+    repo.create_remote("origin", "https://github.com/someone/some-repo.git")
+    assert calkit.detect_project_name() == "someone/some-repo"
+    with open("calkit.yaml", "w") as f:
+        f.write("owner: someone-else\nname: some-project\n")
+    assert calkit.detect_project_name() == "someone-else/some-project"

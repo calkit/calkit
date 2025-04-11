b8a2fe548ead452e0e35d4c6a564bcede2fcf56c
Pete Bachant <petebachant@gmail.com>
2025-04-11 08:54:32 -0700

    Use calkit init in tests

--- calkit/tests/cli/test_main.py
+++ calkit/tests/cli/test_main.py
@@ -14,8 +14,7 @@ from calkit.cli.main import _to_shell_cmd
 
 
 def test_run_in_env(tmp_dir):
-    subprocess.check_call("git init", shell=True)
-    subprocess.check_call("calkit dvc init", shell=True)
+    subprocess.check_call("calkit init", shell=True)
     # First create a new Docker environment for this bare project
     subprocess.check_call(
         "calkit new docker-env "
@@ -110,8 +109,7 @@ def test_run_in_env(tmp_dir):
 
 
 def test_run_in_venv(tmp_dir):
-    subprocess.check_call("git init", shell=True)
-    subprocess.check_call("calkit dvc init", shell=True)
+    subprocess.check_call("calkit init", shell=True)
     # Test uv venv
     subprocess.check_call(
         [

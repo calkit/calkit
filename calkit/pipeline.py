11b3bbfce78b690a3f75147012901367095227d7
Pete Bachant <petebachant@gmail.com>
2025-05-24 07:32:54 -0700

    Start function for pipeline transpiling

---
+++ calkit/pipeline.py
@@ -0,0 +1,22 @@
+"""Pipeline-related functionality."""
+
+import calkit
+from calkit.models.pipeline import Pipeline
+
+
+def to_dvc() -> dict:
+    """Transpile a Calkit pipeline to a DVC pipeline."""
+    ck_info = calkit.load_calkit_info()
+    if "pipeline" not in ck_info:
+        raise ValueError("No pipeline found in calkit.yaml")
+    try:
+        pipeline = Pipeline.model_validate(ck_info["pipeline"])
+    except Exception as e:
+        raise ValueError(f"Pipeline is not defined properly: {e}")
+    dvc_stages = {}
+    # First, create stages for checking all environments
+    # TODO
+    # Now convert Calkit stages into DVC stages
+    for stage_name, stage in pipeline.stages.items():
+        dvc_stages[stage_name] = stage.to_dvc()
+    return dvc_stages

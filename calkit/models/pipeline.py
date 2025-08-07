f2d3c2c55b0c2d1d2e35acb4a6867cb187c1b3c3
Pete Bachant <petebachant@gmail.com>
2025-08-07 11:07:40 -0700

    Start Julia handling

--- calkit/models/pipeline.py
+++ calkit/models/pipeline.py
@@ -48,6 +48,8 @@ class Stage(BaseModel):
         "shell-script",
         "jupyter-notebook",
         "r-script",
+        "julia-script",
+        "word-to-pdf",
     ]
     environment: str
     wdir: str | None = None
@@ -246,6 +248,20 @@ class RScriptStage(Stage):
         for arg in self.args:
             cmd += f" {arg}"
         return cmd
+
+
+class JuliaScriptStage(Stage):
+    kind: Literal["julia-script"] = "julia-script"
+    script_path: str
+
+    @property
+    def dvc_cmd(self) -> str:
+        cmd = f'{self.xenv_cmd} -- include("{self.script_path}")'
+        return cmd
+
+    @property
+    def dvc_deps(self) -> list[str]:
+        return [self.script_path] + super().dvc_deps
 
 
 class JupyterNotebookStage(Stage):

e1cda180162eb3f6479212ef2caccaa7f9cade45
Pete Bachant <petebachant@gmail.com>
2025-08-08 07:18:51 -0700

    Add failing test for complex notebook param types

--- calkit/tests/cli/test_notebooks.py
+++ calkit/tests/cli/test_notebooks.py
@@ -1,8 +1,11 @@
 """Tests for ``calkit.cli.notebooks``."""
 
+import json
 import os
 import shutil
 import subprocess
+
+import calkit
 
 
 def test_clean_notebook_outputs(tmp_dir):
@@ -45,3 +48,33 @@ def test_execute_notebook(tmp_dir):
         ["calkit", "nb", "execute", "main.ipynb", "-e", "main"]
     )
     assert os.path.isfile("results/something.txt")
+    # Test we can execute with parameters
+    nb_fpath = os.path.join(
+        os.path.dirname(__file__), "..", "..", "..", "test", "nb-params.ipynb"
+    )
+    shutil.copy(nb_fpath, "nb-params.ipynb")
+    pipeline = {
+        "stages": {
+            "nb-params": {
+                "kind": "jupyter-notebook",
+                "notebook_path": "nb-params.ipynb",
+                "environment": "main",
+                "parameters": {
+                    "my_value": 5,
+                    "my_list": [1, 2, 3],
+                    "my_dict": {"something": 55.5, "else": "b"},
+                },
+                "html_storage": None,
+            }
+        }
+    }
+    ck_info = calkit.load_calkit_info()
+    ck_info["pipeline"] = pipeline
+    with open("calkit.yaml", "w") as f:
+        calkit.ryaml.dump(ck_info, f)
+    subprocess.check_call(["calkit", "run"])
+    with open("params-out.json") as f:
+        params_out = json.load(f)
+    assert params_out["my_value"] == 5
+    assert params_out["my_list"] == [1, 2, 3]
+    assert params_out["my_dict"] == {"something": 55.5, "else": "b"}

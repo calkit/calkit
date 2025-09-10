2932b311ad1ff4a464f7c985cc6207fa29883ea5
Pete Bachant <petebachant@gmail.com>
2025-09-09 19:45:16 -0700

    Use function in ssh module

--- calkit/cli/main.py
+++ calkit/cli/main.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import csv
-import glob
 import json
 import logging
 import os
@@ -33,6 +32,7 @@ from typing_extensions import Annotated, Optional
 import calkit
 import calkit.matlab
 import calkit.pipeline
+import calkit.ssh
 from calkit.cli import print_sep, raise_error, run_cmd, warn
 from calkit.cli.check import (
     check_app,
@@ -1434,116 +1434,9 @@ def run_in_env(
         except subprocess.CalledProcessError:
             raise_error("Failed to run in julia environment")
     elif env["kind"] == "ssh":
-        try:
-            host = os.path.expandvars(env["host"])
-            user = os.path.expandvars(env["user"])
-            remote_wdir: str = env["wdir"]
-        except KeyError:
-            raise_error(
-                "Host, user, and wdir must be defined for ssh environments"
-            )
-        send_paths = env.get("send_paths")
-        get_paths = env.get("get_paths")
-        key = env.get("key")
-        if key is not None:
-            key = os.path.expanduser(os.path.expandvars(key))
-        remote_shell_cmd = _to_shell_cmd(cmd)
-        # Run with nohup so we can disconnect
-        # TODO: Should we collect output instead of send to /dev/null?
-        remote_cmd = (
-            f"cd '{remote_wdir}' ; nohup {remote_shell_cmd} "
-            "> /dev/null 2>&1 & echo $! "
+        calkit.ssh.run_command(
+            env=env, env_name=env_name, cmd=cmd, verbose=verbose
         )
-        key_cmd = ["-i", key] if key is not None else []
-        # Check to see if we've already submitted a job with this command
-        jobs_fpath = ".calkit/jobs.yaml"
-        job_key = f"{env_name}::{remote_shell_cmd}"
-        remote_pid = None
-        if os.path.isfile(jobs_fpath):
-            with open(jobs_fpath) as f:
-                jobs = calkit.ryaml.load(f)
-            if jobs is None:
-                jobs = {}
-        else:
-            jobs = {}
-        job = jobs.get(job_key, {})
-        remote_pid = job.get("remote_pid")
-        if remote_pid is None:
-            # First make sure the remote working dir exists
-            typer.echo("Ensuring remote working directory exists")
-            subprocess.check_call(
-                ["ssh"]
-                + key_cmd
-                + [f"{user}@{host}", f"mkdir -p {remote_wdir}"]
-            )
-            # Now send any necessary files
-            if send_paths:
-                typer.echo("Sending to remote directory")
-                # Accept glob patterns
-                paths = []
-                for p in send_paths:
-                    paths += glob.glob(p)
-                scp_cmd = (
-                    ["scp", "-r"]
-                    + key_cmd
-                    + paths
-                    + [f"{user}@{host}:{remote_wdir}/"]
-                )
-                if verbose:
-                    typer.echo(f"scp cmd: {scp_cmd}")
-                subprocess.check_call(scp_cmd)
-            # Now run the command
-            typer.echo(f"Running remote command: {remote_shell_cmd}")
-            if verbose:
-                typer.echo(f"Full command: {remote_cmd}")
-            remote_pid = (
-                subprocess.check_output(
-                    ["ssh"] + key_cmd + [f"{user}@{host}", remote_cmd]
-                )
-                .decode()
-                .strip()
-            )
-            typer.echo(f"Running with remote PID: {remote_pid}")
-            # Save PID to jobs database so we can resume waiting
-            typer.echo("Updating jobs database")
-            os.makedirs(".calkit", exist_ok=True)
-            job["remote_pid"] = remote_pid
-            job["submitted"] = time.time()
-            job["finished"] = None
-            jobs[job_key] = job
-            with open(jobs_fpath, "w") as f:
-                calkit.ryaml.dump(jobs, f)
-        # Now wait for the job to complete
-        typer.echo(f"Waiting for remote PID {remote_pid} to finish")
-        ps_cmd = ["ssh"] + key_cmd + [f"{user}@{host}", "ps", "-p", remote_pid]
-        finished = False
-        while not finished:
-            try:
-                subprocess.check_output(ps_cmd)
-                finished = False
-                time.sleep(2)
-            except subprocess.CalledProcessError:
-                finished = True
-                typer.echo("Remote process finished")
-        # Now sync the files back
-        # TODO: Figure out how to do this in one command
-        # Getting the syntax right is troublesome since it appears to work
-        # differently on different platforms
-        if get_paths:
-            typer.echo("Copying files back from remote directory")
-            for src_path in get_paths:
-                src_path = remote_wdir + "/" + src_path  # type: ignore
-                src = f"{user}@{host}:{src_path}"
-                scp_cmd = ["scp", "-r"] + key_cmd + [src, "."]
-                subprocess.check_call(scp_cmd)
-        # Now delete the remote PID from the jobs file
-        typer.echo("Updating jobs database")
-        os.makedirs(".calkit", exist_ok=True)
-        job["remote_pid"] = None
-        job["finished"] = time.time()
-        jobs[job_key] = job
-        with open(jobs_fpath, "w") as f:
-            calkit.ryaml.dump(jobs, f)
     elif env["kind"] == "renv":
         try:
             subprocess.check_call(

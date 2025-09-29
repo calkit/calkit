e9d02bc20e2d4624dd6e4002308283a81a08f04a
Pete Bachant <petebachant@gmail.com>
2025-09-30 00:58:29 +0100

    Add update release command

--- calkit/cli/update.py
+++ calkit/cli/update.py
@@ -8,13 +8,16 @@ import requests
 import typer
 from typing_extensions import Annotated
 
+import calkit
+from calkit.cli import raise_error
+
 update_app = typer.Typer(no_args_is_help=True)
 
 
 @update_app.command(name="devcontainer")
 def update_devcontainer(
     wdir: Annotated[
-        str,
+        str | None,
         typer.Option(
             "--wdir",
             help=(
@@ -37,3 +40,46 @@ def update_devcontainer(
     typer.echo(f"Writing to {out_fpath}")
     with open(out_fpath, "w") as f:
         f.write(resp.text)
+
+
+@update_app.command(name="release")
+def update_release(
+    name: Annotated[str, typer.Option("--name", "-n", help="Release name.")],
+    delete: Annotated[
+        bool, typer.Option("--delete", help="Delete release.")
+    ] = False,
+    publish: Annotated[
+        bool, typer.Option("--publish", help="Publish the release.")
+    ] = False,
+    reupload: Annotated[
+        bool, typer.Option("--reupload", help="Reupload files.")
+    ] = False,
+):
+    """Update a release."""
+    if delete and (publish or reupload):
+        raise_error("Cannot delete release if reuploading or publishing")
+    ck_info = calkit.load_calkit_info()
+    releases = ck_info.get("releases", {})
+    if name not in releases:
+        raise_error(f"Release '{name}' does not exist")
+    release = releases[name]
+    publisher = release.get("publisher")
+    if publisher is None:
+        raise_error("Release does not have a publisher")
+    record_id = release.get("record_id")
+    if record_id is None:
+        raise_error("Release has no record ID")
+    if publish:
+        try:
+            calkit.invenio.post(
+                f"/records/{record_id}/draft/actions/publish",
+                publisher=publisher,
+            )
+        except Exception as e:
+            raise_error(f"Failed to publish release: {e}")
+    if delete:
+        try:
+            calkit.invenio.delete(f"/records/{record_id}/draft")
+        except Exception as e:
+            raise_error(f"Failed to delete release: {e}")
+    # TODO: Finish reupload, add ability to update metadata

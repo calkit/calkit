f25fc198176656d51be80a271d041e44f8a99e1e
Pete Bachant <petebachant@gmail.com>
2025-11-15 18:12:18 -0800

    Add test for latex to-json

---
+++ calkit/tests/cli/test_latex.py
@@ -0,0 +1,56 @@
+"""Tests for ``calkit.cli.latex.``"""
+
+import json
+import os
+import subprocess
+
+from PyPDF2 import PdfReader
+
+
+def test_from_json(tmp_dir):
+    os.makedirs("paper")
+    tex_doc_content = r"""
+\documentclass[11pt]{article}
+\input{results.tex}
+\begin{document}
+This is the document.
+The result is \theresults[result1].
+The other result is \theresults[lol].
+Another is \theresults[sup].
+\end{document}
+"""
+    with open("paper/main.tex", "w") as f:
+        f.write(tex_doc_content)
+    data = {"sup": 5.555, "lol": 3}
+    with open("test.json", "w") as f:
+        json.dump(data, f)
+    fmt_dict = {
+        "result1": "{sup / lol * 1e5 + 22:.1f}",
+        "result2": "sup is {sup} and lol is {lol}",
+        "result3": "{sup * 1e12:.1e}",
+        "lol": "{lol}",
+    }
+    subprocess.run(
+        [
+            "calkit",
+            "latex",
+            "from-json",
+            "test.json",
+            "paper/results.tex",
+            "--command",
+            "theresults",
+            "--format-json",
+            json.dumps(fmt_dict),
+        ],
+        check=True,
+    )
+    subprocess.run(["calkit", "latexmk", "paper/main.tex"], check=True)
+    # Read the generated PDF and check that the values are correct
+    reader = PdfReader("paper/main.pdf")
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    assert "This is the document." in text
+    assert "The result is 185188.7." in text
+    assert "The other result is 3." in text
+    assert "Another is 5.555." in text

525049d8237da3dac81b1f70a92720420d4fde7f
Pete Bachant <petebachant@gmail.com>
2025-12-21 07:09:31 -0800

    Add to notebooks routes

--- calkit/jupyterlab/routes.py
+++ calkit/jupyterlab/routes.py
@@ -1,11 +1,13 @@
 """JupyterLab extension server routes."""
 
+import glob
 import json
 import os
 
 import tornado
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
+from pydantic import BaseModel
 
 import calkit
 
@@ -44,17 +46,83 @@ class KernelspecsRouteHandler(APIHandler):
         self.finish(json.dumps({"kernelspecs": specs}))
 
 
+class Notebook(BaseModel):
+    path: str
+    included_in_project: bool = False
+    included_in_pipeline: bool = False
+    stage_name: str | None = None
+
+
+class NotebooksRouteHandler(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        """Get a list of notebooks in the project.
+
+        We indicate if these are included in the notebooks section and if they
+        are part of the pipeline.
+        """
+        self.log.info("Received request for calkit notebooks info")
+        resp = []
+        # Search for notebooks up to 3 directories deep, but not under .calkit
+        # or .ipynb_checkpoints
+        notebooks = glob.glob("**/*.ipynb", recursive=True)
+        for nb in notebooks:
+            if ".calkit/" in nb or ".ipynb_checkpoints/" in nb:
+                continue
+            resp.append(Notebook(path=nb))
+        # Now check which notebooks are included in the project and pipeline
+        ck_info = calkit.load_calkit_info()
+        project_notebooks = ck_info.get("notebooks", [])
+        for nb in resp:
+            if nb.path in project_notebooks:
+                nb.included_in_project = True
+        stages = ck_info.get("pipeline", {}).get("stages", {})
+        for stage_name, stage in stages.items():
+            if stage.get("kind") == "jupyter-notebook":
+                nb_path = stage.get("notebook_path")
+                for nb in resp:
+                    if nb.path == nb_path:
+                        nb.included_in_pipeline = True
+                        nb.stage_name = stage_name
+        notebooks = [nb.model_dump() for nb in resp]
+        self.finish(json.dumps({"notebooks": notebooks}))
+
+    @tornado.web.authenticated
+    def post(self):
+        """Post a new notebook.
+
+        If the file already exists, add it to the project notebooks and
+        pipeline.
+        """
+        body = self.get_json_body()
+        if not body:
+            self.set_status(400)
+            self.finish(
+                json.dumps({"error": "Request body must be valid JSON"})
+            )
+            return
+        notebook_path = body.get("path")
+        if not notebook_path:
+            self.set_status(400)
+            self.finish(
+                json.dumps({"error": "Request body must include 'path'"})
+            )
+            return
+
+
 def setup_route_handlers(web_app):
     host_pattern = ".*$"
     base_url = web_app.settings["base_url"]
-    hello_route_pattern = url_path_join(base_url, "calkit", "hello")
-    project_route_pattern = url_path_join(base_url, "calkit", "project")
-    kernelspecs_route_pattern = url_path_join(
-        base_url, "calkit", "kernelspecs"
-    )
     handlers = [
-        (hello_route_pattern, HelloRouteHandler),
-        (project_route_pattern, ProjectRouteHandler),
-        (kernelspecs_route_pattern, KernelspecsRouteHandler),
+        (url_path_join(base_url, "calkit", "hello"), HelloRouteHandler),
+        (url_path_join(base_url, "calkit", "project"), ProjectRouteHandler),
+        (
+            url_path_join(base_url, "calkit", "kernelspecs"),
+            KernelspecsRouteHandler,
+        ),
+        (
+            url_path_join(base_url, "calkit", "notebooks"),
+            NotebooksRouteHandler,
+        ),
     ]
     web_app.add_handlers(host_pattern, handlers)

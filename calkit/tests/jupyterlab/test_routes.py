900ccf3691011a9fb3b766bf6bb146e014156a4e
Pete Bachant <petebachant@gmail.com>
2026-01-22 20:32:42 -0800

    Try to chdir in jupyterlab tests

--- calkit/tests/jupyterlab/test_routes.py
+++ calkit/tests/jupyterlab/test_routes.py
@@ -1,16 +1,23 @@
 """Tests for JupyterLab extension server routes."""
 
 import json
+import os
 
 
 async def test_hello(jp_fetch):
-    response = await jp_fetch("calkit", "hello")
-    assert response.code == 200
-    payload = json.loads(response.body)
-    assert payload == {
-        "data": (
-            "Hello, world!"
-            " This is the '/calkit/hello' endpoint."
-            " Try visiting me in your browser!"
-        ),
-    }
+    # Save current working directory to prevent test interference
+    original_cwd = os.getcwd()
+    try:
+        response = await jp_fetch("calkit", "hello")
+        assert response.code == 200
+        payload = json.loads(response.body)
+        assert payload == {
+            "data": (
+                "Hello, world!"
+                " This is the '/calkit/hello' endpoint."
+                " Try visiting me in your browser!"
+            ),
+        }
+    finally:
+        # Restore working directory
+        os.chdir(original_cwd)

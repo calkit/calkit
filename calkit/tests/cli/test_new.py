6b31f73b1dad5a2da6dd42197664edd640887719
Pete Bachant <petebachant@gmail.com>
2026-02-11 07:47:51 -0800

    uv envs don't have prefixes

--- calkit/tests/cli/test_new.py
+++ calkit/tests/cli/test_new.py
@@ -186,7 +186,6 @@ def test_new_uv_env(tmp_dir):
     ck_info = calkit.load_calkit_info()
     env = ck_info["environments"]["my-uv-env"]
     assert env["path"] == ".calkit/envs/my-uv-env/pyproject.toml"
-    assert env["prefix"] == ".calkit/envs/my-uv-env/.venv"
     # Test one in the root of the project
     # TODO: We should make one at the root first if there isn't one, then
     # move into subdirs?
@@ -207,7 +206,6 @@ def test_new_uv_env(tmp_dir):
     ck_info = calkit.load_calkit_info()
     env = ck_info["environments"]["main"]
     assert env["path"] == "pyproject.toml"
-    assert env["prefix"] == ".venv"
 
 
 def test_new_uv_venv(tmp_dir):

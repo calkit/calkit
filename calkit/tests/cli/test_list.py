341da337b8b879b84c26160b70aa557644b4a999
Pete Bachant <petebachant@gmail.com>
2026-03-01 05:32:25 -0800

    Add list stages command (#735)

--- calkit/tests/cli/test_list.py
+++ calkit/tests/cli/test_list.py
@@ -2,6 +2,8 @@
 
 import subprocess
 
+import calkit
+
 
 def test_list_environments(tmp_dir):
     subprocess.check_call("calkit list environments", shell=True)
@@ -10,3 +12,25 @@ def test_list_environments(tmp_dir):
 
 def test_list_templates():
     subprocess.check_call("calkit list templates", shell=True)
+
+
+def test_list_stages(tmp_dir):
+    subprocess.check_call("calkit list stages", shell=True)
+    ck_info = {
+        "pipeline": {
+            "stages": {
+                "stage1": {"kind": "python-script", "script_path": "train.py"},
+                "stage2": {"kind": "shell-command", "command": "echo Hello"},
+            }
+        }
+    }
+    with open("calkit.yaml", "w") as f:
+        calkit.ryaml.dump(ck_info, f)
+    out = subprocess.check_output("calkit list stages", shell=True, text=True)
+    assert "stage1" in out
+    assert "stage2" in out
+    out = subprocess.check_output(
+        "calkit list stages --kind python-script", shell=True, text=True
+    )
+    assert "stage1" in out
+    assert "stage2" not in out

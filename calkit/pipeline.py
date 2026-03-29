8ba9460eb5b06b994599b96916ee20e1ee75e375
Pete Bachant <petebachant@gmail.com>
2026-03-29 09:30:19 -0700

    Handle dvc-zip storage in pipeline.to_dvc

--- calkit/pipeline.py
+++ calkit/pipeline.py
@@ -9,6 +9,7 @@ from pydantic import BaseModel, Field, computed_field, field_validator
 
 import calkit
 import calkit.environments
+import calkit.zips
 from calkit.environments import get_env_lock_fpath
 from calkit.models.iteration import expand_project_parameters
 from calkit.models.pipeline import (
@@ -575,9 +576,11 @@ def to_dvc(
             for out in outputs:
                 if isinstance(out, PathOutput) and out.storage is None:
                     calkit.git.ensure_path_is_ignored(repo, path=out.path)
-                # Unignore path if necessary
                 elif isinstance(out, PathOutput) and out.storage == "git":
                     calkit.git.ensure_path_is_not_ignored(repo, path=out.path)
+                elif isinstance(out, PathOutput) and out.storage == "dvc-zip":
+                    calkit.git.ensure_path_is_ignored(repo, path=out.path)
+                    calkit.zips.add(out.path, is_stage_output=True)
     # Now process any inputs from stage outputs
     for stage_name, stage in pipeline.stages.items():
         for i in stage.inputs:

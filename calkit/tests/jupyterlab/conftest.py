cbf6d17e9b02fc435087ede4f84d939c1aa70bd7
Pete Bachant <petebachant@gmail.com>
2026-06-13 05:27:24 -0700

    Move jupyterlab conftest into its own dir

--- calkit/tests/jupyterlab/conftest.py
+++ calkit/tests/jupyterlab/conftest.py
@@ -1,8 +1,7 @@
 import pytest
 
-pytest_plugins = ("pytest_jupyter.jupyter_server",)
-
 
 @pytest.fixture
 def jp_server_config(jp_server_config):
+    # Load the Calkit server extension in the test Jupyter server.
     return {"ServerApp": {"jpserver_extensions": {"calkit": True}}}
